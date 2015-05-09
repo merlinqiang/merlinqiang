eabef85d1d71c649a081f6211c46ac01abd08bed
MerlinFeng <Merlin.feng@yunzhihui.com>
2015-05-09 14:09:56 +0800

    haoduolizi

---
+++ extensions/tianqi.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <title>地图组件</title>
+    <script src="http://api.map.baidu.com/components?ak=5PruiA8QtP2NhuErHWbEpISi&v=1.0">
+    </script>
+    <script src="jquery-1.7.2.min.js"></script>
+</head>
+<body>
+<iframe class="weather" name="weather_inc" src="http://i.tianqi.com/index.php?c=code&id=2" width="770" height="70" frameborder="0" marginwidth="0" marginheight="0" scrolling="no">
+</iframe>
+<!-- 地图组件 -->
+<lbs-map center="116.403619,39.919851" enable-traffic='true' zoom="12" />
+</body>
+
+</html>
